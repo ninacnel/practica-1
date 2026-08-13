eb24c7d2392c8d4c5100aee467afd3ead0fedf38
nerina <nerinacoronel00@gmail.com>
2026-08-13 14:02:37 -0300

    Added beers file

--- src/App.jsx
+++ src/App.jsx
@@ -2,73 +2,8 @@ import Beers from './components/excercise1/Beers';
 import AvailableBeers from './components/excercise2/AvailableBeers';
 import BeersQuantity from './components/excercise3/BeersQuantity';
 import BeersPrices from './components/excercise4/BeersPrices';
+import { beers } from './components/data/beers';
 import './App.css'
-
-const beers = [
-        {
-            id: 1,
-            beerName: "American",
-            beerStyle: "IPA",
-            price: 2,
-            isAvailable: false,
-        },
-        {
-            id: 2,
-            beerName: "Argenta",
-            beerStyle: "ipa",
-            price: 4,
-            isAvailable: false,
-        },
-        {
-            id: 3,
-            beerName: "Irish",
-            beerStyle: "Red",
-            price: 4,
-            isAvailable: true,
-        },
-        {
-            id: 4,
-            beerName: "Scotish",
-            beerStyle: "Red",
-            price: 3,
-            isAvailable: true,
-        },
-        {
-            id: 5,
-            beerName: "DeEssoCiTratta",
-            beerStyle: "APA",
-            price: 3,
-            isAvailable: true,
-        },
-        {
-            id: 6,
-            beerName: "Santa APA",
-            beerStyle: "APA",
-            price: 3,
-            isAvailable: true,
-        },
-        {
-            id: 7,
-            beerName: "German",
-            beerStyle: "Pilsen",
-            price: 6,
-            isAvailable: true,
-        },
-        {
-            id: 8,
-            beerName: "London Porter",
-            beerStyle: "Porter",
-            price: 2,
-            isAvailable: true,
-        },
-        {
-            id: 9,
-            beerName: "Scottish ALE",
-            beerStyle: "Red",
-            price: 5,
-            isAvailable: false,
-        },
-    ];
 
 function App() {
   return (
